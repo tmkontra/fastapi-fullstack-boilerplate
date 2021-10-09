5ff00417ceb496176b3e81d4b03ac0f895a92bcd
Tyler M. Kontra <46075030+ttymck@users.noreply.github.com>
2021-10-08 22:20:16 -0700

    bug fixes, get_db dependency with warning (#2)

--- migrations/env.py
+++ migrations/env.py
@@ -1,10 +1,13 @@
 from logging.config import fileConfig
 import os
+import sys
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+sys.path.append(".")
 
 from app.database import Base  # noqa
 from app.model import *
